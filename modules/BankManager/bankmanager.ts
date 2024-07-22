b46440b6be6c9f79cd4aea144213d5372eb045a0
TalissaMoura <talissa.ayres@gmail.com>
2024-07-22 11:35:51 -0400

    add bank manager

---
+++ modules/BankManager/bankmanager.ts
@@ -0,0 +1,11 @@
+import ClientAccount from "../Clients/ClientAccount"
+export default class BankManager {
+    _bankManagerID: number
+    _name: String
+    _cpf: String
+    _initDate: string
+    _state: String
+    _city: String
+    _clientAccount: ClientAccount[]
+
+}
