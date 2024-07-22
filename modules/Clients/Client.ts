2de8fe27dd0a080b85060df48509c6b8ef57fa23
TalissaMoura <talissa.ayres@gmail.com>
2024-07-22 11:34:54 -0400

    add clients modules

---
+++ modules/Clients/Client.ts
@@ -0,0 +1,14 @@
+import ClientAccount from "./ClientAccount"
+
+export default class Client {
+    _clientID: number 
+    _name: string
+    _cpf: string
+    _address: string
+    _bornDate: string
+    _state: string
+    _city: string
+    _clientUsername:string
+    _clientPassword: string
+    
+}
