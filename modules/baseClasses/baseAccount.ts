cd6651750e17f46d0d48f0d717baf513e41c90ec
TalissaMoura <talissa.ayres@gmail.com>
2024-07-29 19:54:03 -0400

    update baseAccount abstract class

--- modules/baseClasses/baseAccount.ts
+++ modules/baseClasses/baseAccount.ts
@@ -1,12 +1,16 @@
-import Card from "../Card/cardClass"
 import Client from "../Clients/Client"
 
-export default interface baseAccount {
+export default abstract class baseAccount {
     _client: Client
-    _card: Card
     _isActive: boolean
-    _initDate: string 
+    _initDate: string
+    
+    constructor(client:Client,isActive:boolean,initDate:string){
+        this._client = client
+        this._isActive = isActive
+        this._initDate = initDate
+    }
 
-     getIsActive()
-     setIsActive(new_active:boolean)
+     abstract get isActive()
+     abstract set isActive(new_active:boolean)
 }
