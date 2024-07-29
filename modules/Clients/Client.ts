fc0c2adf054a7561e89226f2d7b8685e9c79ceff
TalissaMoura <talissa.ayres@gmail.com>
2024-07-29 19:41:21 -0400

    modify parameters to be private

--- modules/Clients/Client.ts
+++ modules/Clients/Client.ts
@@ -1,14 +1,31 @@
 import ClientAccount from "./ClientAccount"
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Client {
-    _clientID: number 
-    _name: string
-    _cpf: string
-    _address: string
-    _bornDate: string
-    _state: string
-    _city: string
-    _clientUsername:string
-    _clientPassword: string
+    private _clientID: string
+    private _name: string
+    private _cpf: string
+    private _address: string
+    private _bornDate: string
+    private _state: string
+    private _city: string
+
+    constructor(
+        name:string,
+        cpf: string,
+        address:string,
+        bornDate:string,
+        state:string,
+        city:string,
+    ){
+        this._address = address
+        this._bornDate = bornDate
+        this._clientID = uuidv4()
+        this._name = name 
+        this._cpf = cpf
+        this._state = state
+        this._city = city
+    }
+
     
 }
