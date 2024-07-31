a757be53d68f14aaa4db5f9675a83fe36d4cb32a
TalissaMoura <talissa.ayres@gmail.com>
2024-07-31 19:29:47 -0400

    add old classes to backup

---
+++ backup/modules/Accounts/poupancaAccount.ts
@@ -0,0 +1,51 @@
+import baseAccount from "../baseClasses/baseAccount";
+import Client from "../Clients/Client";
+import { v4 as uuidv4 } from 'uuid';
+
+export default class PoupancaAccount extends baseAccount{
+    _client: Client
+    _isActive: boolean
+    _initDate: string 
+    _poupancaAccountID: number
+    _poupancaAccountNumber:number
+    _poupancaRendimento: number
+    _amount: number
+
+    constructor(
+        client:Client,
+        isActive:boolean,
+        initDate:string,
+        poupancaAccountNumber:number,
+        poupancaRendimento:number){
+        super(client,isActive,initDate)
+        this._client=client
+        this._isActive = isActive
+        this._initDate = initDate
+        this._poupancaAccountID = uuidv4()
+        this._poupancaAccountNumber = poupancaAccountNumber
+        this._poupancaRendimento = poupancaRendimento
+    }
+
+    get amount(){
+        return this._amount
+    }
+    get poupancaAccountNumber(){
+        return this._poupancaAccountNumber
+    }
+
+    get isActive() {
+        return this._isActive
+    }
+    get poupancaRendimento(){
+        return this._poupancaRendimento
+    }
+    set isActive(new_active: boolean) {
+        this._isActive = new_active
+    }
+    set poupancaRendimento(new_rendimento:number){
+        this._poupancaRendimento = new_rendimento
+    }
+    set amount(new_amount:number){
+        this._amount = new_amount
+    }
+}
