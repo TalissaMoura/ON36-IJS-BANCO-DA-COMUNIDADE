df3734554a86020f845393d80cdb77a3030a1324
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:10:39 -0400

    add logic to processTransfer to transferCurrentAccount

--- src/Accounts/products/currentAccount/transferCurrentAccount.model.ts
+++ src/Accounts/products/currentAccount/transferCurrentAccount.model.ts
@@ -1,8 +1,21 @@
+import iAccount from "src/Accounts/factories/iAccount.model.js"
 import iTransfer from "../interfaces/iTransfer.model.js"
+import { UnauthorizedException } from "@nestjs/common"
+import currentAccount from "src/Accounts/creators/currentAccount.model.js"
 
 export default class transferCurrentAccount implements iTransfer {
     
-    processTransfer(){
-        return 
+    processTransfer(amountTransfer:number,currentAccountMakeTransfer:currentAccount,accountToTransfer:iAccount){
+        if(!currentAccountMakeTransfer._isActive && !accountToTransfer._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        else if (currentAccountMakeTransfer._amount < amountTransfer){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const newAmountAccountToTransfer = (accountToTransfer._amount??0) + amountTransfer
+        const newAmountAccountMakeTransfer = (currentAccountMakeTransfer._amount??0) - amountTransfer
+
+        accountToTransfer._amount = newAmountAccountToTransfer
+        currentAccountMakeTransfer._amount = newAmountAccountMakeTransfer
     }
 }
