3e5cbaa5caa1fd5e4707c0dfdbc06f2e531572e7
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:01:55 -0400

    add logic to transfer poupanca account class

--- src/Accounts/products/poupancaAccount/transferPoupancaAccount.model.ts
+++ src/Accounts/products/poupancaAccount/transferPoupancaAccount.model.ts
@@ -1,8 +1,21 @@
+import poupancaAccount from "src/Accounts/creators/poupancaAccount.model.js"
 import iTransfer from "../interfaces/iTransfer.model.js"
+import iAccount from "src/Accounts/factories/iAccount.model.js"
+import { UnauthorizedException } from "@nestjs/common"
 
 export default class transferPoupancaAccount implements iTransfer {
     
-    processTransfer(){
-        return 
+    processTransfer(amountTransfer:number,poupancaAccountMakeTransfer:poupancaAccount,accountToTransfer:iAccount){
+        if(!poupancaAccountMakeTransfer._isActive && !accountToTransfer._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        else if (poupancaAccountMakeTransfer._amount < amountTransfer){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const newAmountAccountToTransfer = (accountToTransfer._amount??0) + amountTransfer
+        const newAmountAccountMakeTransfer = (poupancaAccountMakeTransfer._amount??0) - amountTransfer
+
+        accountToTransfer._amount = newAmountAccountToTransfer
+        poupancaAccountMakeTransfer._amount = newAmountAccountMakeTransfer
     }
 }
