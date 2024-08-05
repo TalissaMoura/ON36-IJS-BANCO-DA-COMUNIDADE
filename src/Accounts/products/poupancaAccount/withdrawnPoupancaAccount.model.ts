71b7ed9d0ce1dc085e86b535d496c87971875b7d
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:02:14 -0400

    add logic to withdrawn poupanca account class

--- src/Accounts/products/poupancaAccount/withdrawnPoupancaAccount.model.ts
+++ src/Accounts/products/poupancaAccount/withdrawnPoupancaAccount.model.ts
@@ -1,7 +1,17 @@
+import poupancaAccount from "src/Accounts/creators/poupancaAccount.model";
 import iWithdrawn from "../interfaces/iWithdrawn.model";
+import { UnauthorizedException } from "@nestjs/common";
 
 export default class withdrawnPoupancaAccount implements iWithdrawn{
-    processWithdrawn():void{
-        return 
+    processWithdrawn(amountWithdrawn:number,account:poupancaAccount):void{
+        if(!account._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        else if (account._amount < amountWithdrawn){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const new_amount = account._amount - amountWithdrawn 
+        
+        account._amount = new_amount
     }
 }
