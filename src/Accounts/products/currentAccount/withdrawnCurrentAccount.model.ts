bc2578d6d508a327eb90a31f2429a6c94814a87f
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:10:11 -0400

    add logic to processWithdrawn to withdrawnCurrentAccount

--- src/Accounts/products/currentAccount/withdrawnCurrentAccount.model.ts
+++ src/Accounts/products/currentAccount/withdrawnCurrentAccount.model.ts
@@ -1,8 +1,18 @@
+import currentAccount from "src/Accounts/creators/currentAccount.model.js"
 import iWithdrawn from "../interfaces/iWithdrawn.model.js"
+import { UnauthorizedException } from "@nestjs/common"
 
-export default class withdrawnCurrentaAccount implements iWithdrawn {
+export default class withdrawnCurrentAccount implements iWithdrawn {
     
-    processWithdrawn(){
-        return 
+    processWithdrawn(amountWithdrawn:number,account:currentAccount){
+        if(!account._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        else if (account._amount < amountWithdrawn){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const new_amount = account._amount - amountWithdrawn 
+        
+        account._amount = new_amount
     }
 }
