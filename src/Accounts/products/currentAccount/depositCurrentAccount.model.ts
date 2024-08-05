7d2e95dd3f78b964b876a92889cd79b25c30304d
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:08:16 -0400

    add logic to processDeposit to depositCurrentAccount

--- src/Accounts/products/currentAccount/depositCurrentAccount.model.ts
+++ src/Accounts/products/currentAccount/depositCurrentAccount.model.ts
@@ -1,8 +1,15 @@
+import currentAccount from "src/Accounts/creators/currentAccount.model.js"
 import iDeposit from "../interfaces/iDeposit.model.js"
+import { UnauthorizedException } from "@nestjs/common"
 
 export default class depositCurrentAccount implements iDeposit {
     
-    processDeposit(){
-        return 
+    processDeposit(amountDeposit:number,currentAccount:currentAccount){
+        if(!currentAccount._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const new_amount = (currentAccount._amount?? 0) + amountDeposit
+
+        currentAccount._amount = new_amount
     }
 }
