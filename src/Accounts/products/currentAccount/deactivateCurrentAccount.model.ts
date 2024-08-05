8c0e51da048568cd9863e61e9eace8e21287f1e5
TalissaMoura <talissa.ayres@gmail.com>
2024-08-04 22:03:51 -0400

    update deactivateCurrentAccount

--- src/Accounts/products/currentAccount/deactivateCurrentAccount.model.ts
+++ src/Accounts/products/currentAccount/deactivateCurrentAccount.model.ts
@@ -1,7 +1,8 @@
+import currentAccount from "src/Accounts/creators/currentAccount.model";
 import iDeactivateAccount from "../interfaces/iDeactivateAccount.model"
 
 export default class deactivateCurrentAccount implements iDeactivateAccount{
-    deactivate():void {
-        
+    deactivate(account:currentAccount):void {
+        account._isActive = false
     }
 }
