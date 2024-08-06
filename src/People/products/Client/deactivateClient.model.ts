d40ae1c8897b34e950be8b2215dbc76198f3ce67
TalissaMoura <talissa.ayres@gmail.com>
2024-08-05 22:57:00 -0400

    add deactivateClient product class

---
+++ src/People/products/Client/deactivateClient.model.ts
@@ -0,0 +1,8 @@
+import Client from "../../creators/client.model";
+import iDeactivatePeopleType from "../interfaces/iDeactivatePeopleType.model";
+
+export default class deactivateClient implements iDeactivatePeopleType{
+    deactivate(account:Client){
+        account._isActive = false
+    }
+}
