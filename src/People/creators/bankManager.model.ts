66b2f9b645388db312b52b0a0a4396d26e474d20
TalissaMoura <talissa.ayres@gmail.com>
2024-08-05 22:55:19 -0400

    add bankManager concrete class

--- src/People/creators/bankManager.model.ts
+++ src/People/creators/bankManager.model.ts
@@ -1,7 +1,6 @@
 import iPeople from "../factory/iPeople.model";
-import { v4 as uuidv4 } from 'uuid';
-import deactivateClientAccount from "../products/ClientAccount/deactivateClientAccount.model";
-import clientAccount from "src/clientAccount/clientAccount.model";
+import createBankManager from "../products/BankManager/createBankManager.model";
+import deactivateBankManager from "../products/BankManager/deactivateBankManager.model";
 
 export default class bankManager implements iPeople {
     _peopleID: string 
@@ -10,22 +9,11 @@ export default class bankManager implements iPeople {
     _isActive: boolean
     _bankManagerToken: string
 
-    constructor(
-        name:string,
-        cpf:string,
-        isActive:boolean
-    ){
-        this._peopleID = uuidv4()
-        this._name = name 
-        this._cpf = cpf
-        this._isActive = isActive
-        this._bankManagerToken = uuidv4()
+    createAccount():createBankManager {
+        return new createBankManager()
     }
-
-    private validateOperation(token:string){
-        if (token !== this._bankManagerToken){
-            return false
-        }
-        return true
+    deactivateAccount(): deactivateBankManager{
+        return new deactivateBankManager()
     }
 }
+
