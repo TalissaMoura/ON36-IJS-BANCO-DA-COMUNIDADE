6fc44476d8674ce56cc56d82961c4a3f24f724b1
TalissaMoura <talissa.ayres@gmail.com>
2024-08-05 22:46:38 -0400

    delete old files

--- src/People/products/ClientAccount/createClientAccount.model.ts
+++
@@ -1,9 +0,0 @@
-import clientAccount from "src/People/creators/clientAccount.model";
-import iCreatePeopleType from "../interfaces/iCreatePeopleType.model";
-
-export default class createClientAccount implements iCreatePeopleType {
-    create(accessToken:string){
-
-        return new clientAccount()
-    }
-}
