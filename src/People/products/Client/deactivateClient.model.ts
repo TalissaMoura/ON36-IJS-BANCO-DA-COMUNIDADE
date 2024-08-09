da71a8e782c198156216fd1f61d6580e73257500
TalissaMoura <talissa.ayres@gmail.com>
2024-08-09 13:54:43 -0400

    update method deactivate

--- src/People/products/Client/deactivateClient.model.ts
+++ src/People/products/Client/deactivateClient.model.ts
@@ -4,5 +4,6 @@ import iDeactivatePeopleType from "../interfaces/iDeactivatePeopleType.model";
 export default class deactivateClient implements iDeactivatePeopleType{
     deactivate(account:Client){
         account._isActive = false
+        return account
     }
 }
