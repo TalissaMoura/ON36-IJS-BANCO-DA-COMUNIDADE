9ac45c3d2c806bee8ec175fe0df2f07472b48865
TalissaMoura <talissa.ayres@gmail.com>
2024-08-09 13:52:14 -0400

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { BankManagerModule } from './bankManager/bankManager.module';
 import { bankManagerController } from './bankManager/bankManager.controller';
 import { bankManagerService } from './bankManager/bankManager.service';
+import { PeopleService } from './People/people.service';
+import { PeopleController } from './People/people.controller';
 
 @Module({
 
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PeopleController],
+  providers: [AppService, PeopleService],
   imports: [BankManagerModule],
 })
 export class AppModule {}
