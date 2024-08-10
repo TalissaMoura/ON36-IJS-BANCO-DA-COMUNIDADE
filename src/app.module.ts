bd4259de4c2132866a543ece70cddc0626d9fc4e
TalissaMoura <talissa.ayres@gmail.com>
2024-08-09 22:48:02 -0400

    add controllers and service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { bankManagerController } from './People/bankManager.controller';
 import { PeopleService } from './People/people.service';
 import { clientController } from './People/client.controller';
+import { currentAccountController } from './Accounts/currentAccount.controller';
+import { poupancaAccountController } from './Accounts/poupancaAccount.controller';
+import { AccountsService } from './Accounts/accounts.service';
 
 @Module({
 
-  controllers: [AppController, bankManagerController,clientController],
-  providers: [AppService,PeopleService],
+  controllers: [AppController, bankManagerController,clientController,currentAccountController,poupancaAccountController],
+  providers: [AppService,PeopleService,AccountsService],
 })
 export class AppModule {}
