b4c7f8fa3ce2eade92549995b99ac65ab468ce99
TalissaMoura <talissa.ayres@gmail.com>
2024-08-13 22:11:42 -0400

    update currentAccount creator

--- src/Accounts/creators/currentAccount.model.ts
+++ src/Accounts/creators/currentAccount.model.ts
@@ -1,18 +1,28 @@
-import iAccount from "../factories/iAccount.model";
-import depositCurrentAccount from "../products/currentAccount/depositCurrentAccount.model"
-import transferCurrentAccount from "../products/currentAccount/transferCurrentAccount.model";
-import withdrawnCurrentAccount from "../products/currentAccount/withdrawnCurrentAccount.model";
-import openCurrentAccount from "../products/currentAccount/openCurrentAccount.model";
 import { UnauthorizedException } from "@nestjs/common";
-import deactivateCurrentAccount from "../products/currentAccount/deactivateCurrentAccount.model";
+import poupancaAccount from "./poupancaAccount.model";
+import iDeposit from "../products/iDeposit.model";
+import iWithdrawn from "../products/iWithdrawn.model";
+import iTransfer from "../products/iTransfer.model";
+import iDeactivateAccount from "../products/iDeactivateAccount.model";
+import iAccount from "../products/iAccount.model";
+import { v4 as uuidv4 } from 'uuid';
 
-export default class currentAccount implements iAccount {
+
+export default class currentAccount implements iAccount, iDeposit,iWithdrawn,iTransfer,iDeactivateAccount {
     _accountID: string 
     _accountNumber: string 
     _amount: number
     _isActive: boolean
     _initDate: string
     _limitChequeEspecial:number
+
+    constructor(amount:number,initDate:string){
+        this._isActive = true 
+        this._initDate = initDate
+        this._amount = amount 
+        this._accountID = uuidv4()
+        this._accountNumber = uuidv4()
+    }
     
     private validateOperation(isBankManager:boolean){
         if(!isBankManager){
@@ -20,27 +30,43 @@ export default class currentAccount implements iAccount {
         }
         return true
     }
-
-    createOpenAccount(isBankManager:boolean):openCurrentAccount{
-        if(!this.validateOperation(isBankManager)){
-            throw new UnauthorizedException("Unauthorized operation")
+    processDeposit(amountDeposit: number): void {
+        if(!this._isActive){
+            throw new UnauthorizedException("Invalid operation")
         }
-        return  new openCurrentAccount()
-    }
+        const new_amount = (this._amount?? 0) + amountDeposit
 
-    createDeposit(): depositCurrentAccount {
-        return new depositCurrentAccount()
+        this._amount = new_amount
     }
-    createTransfer(): transferCurrentAccount {
-        return new transferCurrentAccount()
+    processTransfer(
+        amountTransfer:number,
+        accountToTransfer:poupancaAccount|currentAccount): void {
+            if(!this._isActive && !accountToTransfer._isActive){
+                throw new UnauthorizedException("Invalid operation")
+            }
+            else if (this._amount < amountTransfer){
+                throw new UnauthorizedException("Invalid operation")
+            }
+            const newAmountAccountToTransfer = (accountToTransfer._amount??0) + amountTransfer
+            const newAmountAccountMakeTransfer = (this._amount??0) - amountTransfer
+    
+            accountToTransfer._amount = newAmountAccountToTransfer
+            this._amount = newAmountAccountMakeTransfer
     }
-    createWithdrawn(): withdrawnCurrentAccount {
-        return new withdrawnCurrentAccount()
+    processWithdrawn(amountWithdrawn:number): void {
+        if(!this._isActive){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        else if (this._amount < amountWithdrawn){
+            throw new UnauthorizedException("Invalid operation")
+        }
+        const new_amount = this._amount - amountWithdrawn 
+        
+        this._amount = new_amount
     }
-    startDeactivate():deactivateCurrentAccount{
-        return new deactivateCurrentAccount()
+    processDeactivate(): void {
+        this._isActive = false
     }
-
     updateLimitChequeEspecial(isBankManager:boolean,limitValue:number):void{
         if(!this.validateOperation(isBankManager)){
             throw new UnauthorizedException("Unauthorized operation")
