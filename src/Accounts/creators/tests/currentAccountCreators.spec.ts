00d5c541c2de22b061e64202055c9148acec04af
TalissaMoura <talissa.ayres@gmail.com>
2024-08-13 22:13:32 -0400

    add currentAccount tests

--- src/Accounts/creators/tests/currentAccountCreators.spec.ts
+++ src/Accounts/creators/tests/currentAccountCreators.spec.ts
@@ -1,30 +1,49 @@
-import { UnauthorizedException } from "@nestjs/common";
-import openCurrentAccount from "../../products/currentAccount/openCurrentAccount.model";
 import currentAccount from "../currentAccount.model";
-import depositCurrentAccount from "../../products/currentAccount/depositCurrentAccount.model";
-import transferCurrentAccount from "../../products/currentAccount/transferCurrentAccount.model";
-import withdrawnCurrentaAccount from "../../products/currentAccount/withdrawnCurrentAccount.model";
+import poupancaAccount from "../poupancaAccount.model";
+
+jest.mock('uuid', () => ({
+    v4: jest.fn(),
+  }));
 
 
 describe("currentAccount products tests",()=>{
-    const account: currentAccount  = new currentAccount()
-    test("should return an OpenAccount object", () =>{
-        expect(account.createOpenAccount(true)).toBeInstanceOf(openCurrentAccount)
+    test("should make a transfer from currentAccount to currentAccount", () =>{
+        const accountA: currentAccount  = new currentAccount(100,"2023-12-12")
+        const accountB: currentAccount  = new currentAccount(150,"2023-12-10")
+        accountA.processTransfer(20,accountB)
+        expect(accountA).toHaveProperty("_amount",80)
+        expect(accountB).toHaveProperty("_amount",170)        
     })
-    test("should throw an Unauthorized operation if bankManager is false", () => {
-        expect(()=>account.createOpenAccount(false)).toThrow(UnauthorizedException)
+    test("should make a transfer from currentAccount to popuancaAccount", () =>{
+        const accountA: currentAccount  = new currentAccount(450,"2023-10-10")
+        const accountB: poupancaAccount  = new poupancaAccount(160,"2006-8-10")
+        accountA.processTransfer(20,accountB)
+        expect(accountA).toHaveProperty("_amount",430)
+        expect(accountB).toHaveProperty("_amount",180)        
     })
-    test("create new current account desposit", () => {
-        expect(account.createDeposit()).toBeInstanceOf(depositCurrentAccount)
+    test("should make a deposit", () => {
+        const accountA: currentAccount  = new currentAccount(120,"2021-12-12")
+        accountA.processDeposit(10)
+        expect(accountA).toHaveProperty("_amount",130)
     })
-    test("create new current account transfer", () => {
-        expect(account.createTransfer()).toBeInstanceOf(transferCurrentAccount)
+    test("should make a withdrawn ", () => {
+        const accountA: currentAccount  = new currentAccount(100,"2022-9-12")
+        accountA.processWithdrawn(20)
+        expect(accountA).toHaveProperty("_amount",80)
     })
-    test("create new current account transfer", () => {
-        expect(account.createWithdrawn()).toBeInstanceOf(withdrawnCurrentaAccount)
+    test("should deactivate a account", () => {
+        const accountA: currentAccount  = new currentAccount(110,"2022-10-12")
+        accountA.processDeactivate()
+        expect(accountA).toHaveProperty("_isActive",false)
     })
-    test("should set a new limitChequeEspecial", () => {
-        account.updateLimitChequeEspecial(true,200)
-        expect(account).toHaveProperty("_limitChequeEspecial",200)
+    test("should update limitChequeEspecial", () => {
+        const accountA: currentAccount  = new currentAccount(110,"2022-10-12")
+        accountA.updateLimitChequeEspecial(true,200)
+        expect(accountA).toHaveProperty("_limitChequeEspecial",200)
+    })
+    test.failing("should not update limitChequeEspecial", () => {
+        const accountA: currentAccount  = new currentAccount(110,"2022-10-12")
+        accountA.updateLimitChequeEspecial(false,200)
+        expect(accountA).toHaveProperty("_limitChequeEspecial",200)
     })
 })
