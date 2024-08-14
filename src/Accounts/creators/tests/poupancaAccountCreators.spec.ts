fb1a42a1655bb33e0bf8db9a34d52b0b61354308
TalissaMoura <talissa.ayres@gmail.com>
2024-08-13 22:13:40 -0400

    add poupancaAccount tests

--- src/Accounts/creators/tests/poupancaAccountCreators.spec.ts
+++ src/Accounts/creators/tests/poupancaAccountCreators.spec.ts
@@ -1,30 +1,49 @@
-import { UnauthorizedException } from "@nestjs/common";
+import currentAccount from "../currentAccount.model";
 import poupancaAccount from "../poupancaAccount.model";
-import openPoupancaAccount from "../../products/poupancaAccount/openPoupancaAccount.model";
-import depositPoupancaAccount from "../../products/poupancaAccount/depositPoupancaAccount.model";
-import transferPoupancaAccount from "../../products/poupancaAccount/transferPoupancaAccount.model";
-import withdrawnPoupancaAccount from "../../products/poupancaAccount/withdrawnPoupancaAccount.model";
+
+jest.mock('uuid', () => ({
+    v4: jest.fn(),
+  }));
 
 
-describe("PoupancaAccount products tests",()=>{
-    const account: poupancaAccount  = new poupancaAccount()
-    test("should return an OpenAccount object", () =>{
-        expect(account.createOpenAccount(true)).toBeInstanceOf(openPoupancaAccount)
+describe("poupancaAccount products tests",()=>{
+    test("should make a transfer from poupancaAccount to currentAccount", () =>{
+        const accountA: poupancaAccount  = new poupancaAccount(100,"2023-12-12")
+        const accountB: currentAccount  = new currentAccount(150,"2023-12-10")
+        accountA.processTransfer(20,accountB)
+        expect(accountA).toHaveProperty("_amount",80)
+        expect(accountB).toHaveProperty("_amount",170)        
     })
-    test("should throw an Unauthorized operation if bankManager is false", () => {
-        expect(()=>account.createOpenAccount(false)).toThrow(UnauthorizedException)
+    test("should make a transfer from poupancaAccount to popuancaAccount", () =>{
+        const accountA: poupancaAccount  = new poupancaAccount(450,"2023-10-10")
+        const accountB: poupancaAccount  = new poupancaAccount(160,"2006-8-10")
+        accountA.processTransfer(20,accountB)
+        expect(accountA).toHaveProperty("_amount",430)
+        expect(accountB).toHaveProperty("_amount",180)        
     })
-    test("create new Poupanca account desposit", () => {
-        expect(account.createDeposit()).toBeInstanceOf(depositPoupancaAccount)
+    test("should make a deposit", () => {
+        const accountA: poupancaAccount  = new poupancaAccount(120,"2021-12-12")
+        accountA.processDeposit(10)
+        expect(accountA).toHaveProperty("_amount",130)
     })
-    test("create new Poupanca account transfer", () => {
-        expect(account.createTransfer()).toBeInstanceOf(transferPoupancaAccount)
+    test("should make a withdrawn ", () => {
+        const accountA: poupancaAccount  = new poupancaAccount(100,"2022-9-12")
+        accountA.processWithdrawn(20)
+        expect(accountA).toHaveProperty("_amount",80)
     })
-    test("create new Poupanca account transfer", () => {
-        expect(account.createWithdrawn()).toBeInstanceOf(withdrawnPoupancaAccount)
+    test("should deactivate a account", () => {
+        const accountA: poupancaAccount  = new poupancaAccount(110,"2022-10-12")
+        accountA.processDeactivate()
+        expect(accountA).toHaveProperty("_isActive",false)
     })
-    test("should set a new rendimento", () => {
-        account.updateRendimento(true,0.2)
-        expect(account).toHaveProperty("_rendimento",0.2)
+    test("should update rendimento", () => {
+        const accountA: poupancaAccount  = new poupancaAccount(110,"2022-10-12")
+        accountA.updateRendimento(true,0.25)
+        expect(accountA).toHaveProperty("_rendimento",0.25)
+    })
+    test.failing("should not update rendimento", () => {
+        const accountA: poupancaAccount  = new poupancaAccount(110,"2022-10-12")
+        accountA.updateRendimento(false,0.25)
+        expect(accountA).toHaveProperty("_rendimento",200)
     })
 })
