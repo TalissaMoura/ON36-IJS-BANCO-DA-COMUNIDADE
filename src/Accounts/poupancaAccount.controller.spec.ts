9d6f9fb9ea806697b40831d6575a05c04ae5333a
TalissaMoura <talissa.ayres@gmail.com>
2024-08-15 19:51:42 -0400

    update tests

--- src/Accounts/poupancaAccount.controller.spec.ts
+++ src/Accounts/poupancaAccount.controller.spec.ts
@@ -16,47 +16,43 @@ describe('poupancaAccountController', () => {
   
     test('should create a poupanca account', () => {
       const isBankManager = true
-      const amount = 2024;
-      const initDate = '2024-11-20';
-      const accountNumber = 4569
+      const amount = 2076;
+      const initDate = '2022-09-10';
   
   
       return supertest(app.getHttpServer())
         .post('/poupancaAccounts')
         .send({
          isBankManager,
-         accountNumber,
          amount,
          initDate,
         })
         .expect(201)
         .expect(({ body }) => {
+          expect(body._isActive).toBe(true);
           expect(body._amount).toBe(amount);
           expect(body._initDate).toBe(initDate);
-          expect(body._accountNumber).toBe(accountNumber);
         });
     });
   
     test('should return a poupanca account', () => {
-      const id = '31644825-b413-4f26-9d21-e88d42072813';
+      const id = '2c6c8f26-36cd-446b-951c-0e039cbfad14';
   
   
       return supertest(app.getHttpServer())
         .get(`/poupancaAccounts/${id}`)
         .expect(200)
         .expect(({ body }) => {
-          body._amount = 256
-          body._initDate = "2023-7-21"
+          body._amount = 250
+          body._initDate = "2024-5-8"
           body._isActive = true
-          body._accountNumber= "8965"
         });
     });
   
     test('should update a poupanca accounts', () => {
-      const id = '54c84d54-64c1-4583-bb51-0939a8d007b1';
+      const id = '6ce73870-ead1-4807-907e-73d1f6fed7fa';
       const amount = 2250
-      const initDate = "2022-4-14"
-      const accountNumber = "7412"
+      const initDate = "2022-8-16"
   
       return supertest(app.getHttpServer())
         .put(`/poupancaAccounts/${id}`)
@@ -64,19 +60,17 @@ describe('poupancaAccountController', () => {
           id,
           amount,
           initDate,
-          accountNumber
         })
         .expect(200)
         .expect(({ body }) => {
           body._amount = amount
           body._initDate = initDate
           body._isActive = true
-          body._accountNumber = accountNumber
         });
     });
   
     test('should deactivate a current account', () => {
-      const id = 'dd8d8b0a-dd24-4920-a19e-0aa5d3424471';
+      const id = '797bc9f0-f344-47c4-9b45-96b42233a764';
   
       return supertest(app.getHttpServer())
         .delete(`/poupancaAccounts/${id}`)
