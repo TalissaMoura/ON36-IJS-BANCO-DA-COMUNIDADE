77916696c94624457aca3566980c7f1c48467575
TalissaMoura <talissa.ayres@gmail.com>
2024-08-18 22:47:26 -0400

    update controller methods to be async

--- src/Accounts/poupancaAccount.controller.ts
+++ src/Accounts/poupancaAccount.controller.ts
@@ -13,27 +13,27 @@ export class poupancaAccountController {
     @Body('isBankManager') isBankManager: boolean,
     @Body('amount') amount: number,
     @Body("initDate") initDate: string
-    ): poupancaAccount {
+    ): Promise<poupancaAccount> {
       const account = this.accountService.createAccount(isBankManager,typeAccount.POUPANCA,amount,initDate);
-    return account as poupancaAccount
+    return account as Promise<poupancaAccount>
   }
     @Get(":id")
-    findpoupancaAccountByID(
-      @Param('id',new ParseUUIDPipe({ version: '4' })) id: string): poupancaAccount {
-      return this.accountService.findPoupancaAccountById(id)
+    async findpoupancaAccountByID(
+      @Param('id',new ParseUUIDPipe({ version: '4' })) id: string): Promise<poupancaAccount> {
+        return await this.accountService.findPoupancaAccountById(id)
     }
     @Put(':id')
-    updatepoupancaAccountById(
+    async updatepoupancaAccountById(
       @Param('id',new ParseUUIDPipe({ version: '4' })) id: string,
       @Body("initDate") initDate: string,
       @Body("amount") amount: number
-    ): poupancaAccount{
-      return this.accountService.updatePoupancaAccountById(id,amount,initDate)
+    ): Promise<poupancaAccount>{
+      return await this.accountService.updatePoupancaAccountById(id,amount,initDate)
     }
     @Delete(":id")
-    deactivateClientByID(
+    async deactivateClientByID(
       @Param("id",new ParseUUIDPipe({ version: '4' })) id: string
-    ):poupancaAccount{
-      return this.accountService.deactivatePoupancaAccountById(id)
+    ):Promise<poupancaAccount>{
+      return await this.accountService.deactivatePoupancaAccountById(id)
     }
 }
