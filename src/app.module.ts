4230bb17348d1ecb503579dea7e6ff1f296e5002
TalissaMoura <talissa.ayres@gmail.com>
2024-08-18 22:48:12 -0400

    add repository to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { clientController } from './People/client.controller';
 import { currentAccountController } from './Accounts/currentAccount.controller';
 import { poupancaAccountController } from './Accounts/poupancaAccount.controller';
 import { AccountsService } from './Accounts/accounts.service';
+import { currentAccountJsonFileRepository } from './Accounts/currentAccountRepository';
+import { poupancaAccountJsonFileRepository } from './Accounts/poupancaAccountRepository';
 
 @Module({
-
   controllers: [AppController, bankManagerController,clientController,currentAccountController,poupancaAccountController],
-  providers: [AppService,PeopleService,AccountsService],
+  providers: [AppService,PeopleService,AccountsService,currentAccountJsonFileRepository,poupancaAccountJsonFileRepository],
 })
 export class AppModule {}
